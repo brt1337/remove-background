16653a3504240cf29c08ce0eb1430f013b247d33
brt1337 <57838619+brt1337@users.noreply.github.com>
2023-03-14 22:47:18 -0300

    Update youtube_video.py

--- pages/youtube_video.py
+++ pages/youtube_video.py
@@ -6,7 +6,7 @@ import ssl
 ssl._create_default_https_context = ssl._create_stdlib_context
 
 
-st.set_page_config(page_title="Download Video", page_icon="💻", layout="centered",initial_sidebar_state="collapsed")
+st.set_page_config(page_title="Download Video", page_icon="💻", layout="centered")
 hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
